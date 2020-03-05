c2ffd44e4e643310522e4caa3f53561d51c020fa
Arunmozhi <arun@arunmozhi.in>
2020-03-06 05:11:13 +0530

    updated ger viz

--- pages/education/dropout.vue
+++ pages/education/dropout.vue
@@ -1,9 +1,6 @@
 <template>
-  <section class="container">
-    <div class="py-4">
-      <div class="px-4 inline font-light tracking-wider">VISUALIZATIONS</div>
-      <a href="/" class="px-4"><font-awesome-icon icon="home" /> Home</a>
-    </div>
+  <section class="container section">
+    <h1 class="title">Gross Enrollment Ratio Across States</h1>
     <div id="dia">
       <svg ref="viz" :width="width" :height="height"></svg>
     </div>
@@ -20,7 +17,13 @@ export default {
       circles: [{ x: 70, y: 50, r: 30 }, { x: 170, y: 100, r: 50 }],
       csv: [],
       svg: null,
-      width: 900
+      width: 900,
+      xColumns: [
+        'Primary_Total',
+        'Upper_Primary_Total',
+        'Secondary_Total',
+        'Higher_Secondary_Total'
+      ]
     }
   },
   computed: {
@@ -28,7 +31,7 @@ export default {
       return this.csv.map(row => row.State_UT)
     },
     height: function() {
-      return this.csv.length * 25
+      return this.states.length * 25 + 50
     }
   },
   mounted() {
@@ -39,44 +42,77 @@ export default {
     async fetchGerData() {
       const data = await this.$axios.$get('/data/GER_1.csv')
       this.csv = d3.csvParse(data)
-      this.renderNames()
-      this.drawStateSeparators()
+      this.renderLabels()
+      this.draw()
     },
-    renderNames() {
+    renderLabels() {
       this.svg
         .selectAll('text')
         .data(this.states)
         .enter()
         .append('text')
-        .attr('x', 0)
-        .attr('y', (d, i) => (i + 1) * 25)
+        .attr('x', 140)
+        .attr('y', (d, i) => (i + 2) * 25 - 5)
         .text(d => d)
-        .attr('font-size', '10px')
+        .attr('font-size', '11px')
         .attr('font-family', 'sans-serif')
-        .attr('fill', 'purple')
+        .attr('text-anchor', 'end')
     },
-    drawStateSeparators() {
+    draw() {
       // const seps = this.svg.select('g').append('g#seperators')
+      const x = d3
+        .scaleBand()
+        .domain(this.xColumns)
+        .range([150, this.width])
+
+      const y = d3
+        .scaleLinear()
+        .domain([0, 100])
+        .range([0, 24])
+
       for (let i = 0; i < this.states.length; i++) {
+        const row = this.csv[i]
+        const data = this.xColumns.map(k => ({
+          key: k,
+          value: row[k]
+        }))
+
+        /*
+        const colorSclae = d3
+          .scaleLinear()
+          .domain([50, 100])
+          .range([0, 1])
+        */
+
         this.svg
-          .append('line')
-          .attr('x1', 150)
-          .attr('x2', this.width)
-          .attr('y1', (i + 1) * 25 + 8)
-          .attr('y2', (i + 1) * 25 + 8)
-          .attr('stroke', 'red')
-          .attr('stroke-width', 0.3)
-          .attr('opacity', 0.3)
+          .append('g')
+          .attr('id', this.states[i])
+          .selectAll('rect')
+          .data(data)
+          .join('rect')
+          .attr('x', d => x(d.key))
+          .attr('y', d => (i + 2) * 25 - (y(d.value) < 24 ? y(d.value) : 24))
+          .attr('height', d => (y(d.value) < 24 ? y(d.value) : 24))
+          .attr('width', x.bandwidth())
+          //  .attr('fill', d => d3.interpolateRdYlGn(colorSclae(d.value)))
+          .attr('fill', 'steelblue')
       }
       this.svg
-        .append('line')
-        .attr('x1', 150)
-        .attr('x2', 150)
-        .attr('y1', 0)
-        .attr('y2', this.height)
-        .attr('stroke', 'red')
-        .attr('stroke-width', 0.3)
-        .attr('opacity', 0.3)
+        .append('g')
+        .selectAll('text')
+        .data(this.xColumns)
+        .enter()
+        .append('text')
+        .attr('x', d => x(d) + x.bandwidth() / 2)
+        .attr('y', 15)
+        .text(d => {
+          const t = d.split('_')
+          t.pop()
+          return t.join(' ')
+        })
+        .attr('font-size', '11px')
+        .attr('font-family', 'sans-serif')
+        .attr('text-anchor', 'middle')
     }
   }
 }
